3baf437d5b7e127763381ec4f78d71aa35e49288
Anand Singh <04xia3anandsingh@gmail.com>
2026-07-01 01:28:46 +0530

    Initial commit

---
+++ DeepSkilling/Week1/Module 1 - Design Patterns and Principles/DependencyInjectionExample/DependencyInjectionTest.java
@@ -0,0 +1,14 @@
+public class DependencyInjectionTest {
+
+    public static void main(String[] args) {
+
+        // Create Repository
+        CustomerRepository repository = new CustomerRepositoryImpl();
+
+        // Inject Repository into Service
+        CustomerService service = new CustomerService(repository);
+
+        // Find Customer
+        service.getCustomerDetails(101);
+    }
+}
